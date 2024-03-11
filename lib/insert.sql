79c33f7117104ccda1e45e615090d878109e7a3a
+collins <+collinskipngetich60@gmail.com>
2024-03-11 09:26:01 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex) VALUES
+('Mr. Chocolate', 5, 'M'),
+('Rowdy', 3, 'M'),
+('Tabitha', 2, 'F'),
+('Sergeant Brown', 7, 'M'),
+('Melissa', 4, 'F'),
+('Grinch', 6, 'M'),
+('Wendy', 8, 'F'),
+('unnamed', NULL, 'F');
